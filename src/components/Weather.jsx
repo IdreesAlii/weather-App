6797b86792824707e65e8d98708baf974ecff5e1
IdreesAl <idreesalihassan2008@gmail.com>
2025-07-21 11:56:34 +0500

    took the useEffect wasnt using it before

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import './Weather.css'
 import search_icon from '../Assets/search.png'
 import clear_icon from '../Assets/clear.png'
